6667143ce7a073c36c1308d7bef931d4764d170a
Douglas Castillo <douglascastillo087@gmail.com>
2021-10-04 19:12:59 -0600

    Add project files.

---
+++ FitProAPI/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Dapper;
+using FitProAPI.Models;
+using FitProAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitProAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IDapper _dapper;
+        public HomeController(IDapper dapper)
+        {
+            _dapper = dapper;
+        }
+        [HttpPost(nameof(Create))]
+        public async Task<int> Create(Customer data)
+        {
+            var dbparams = new DynamicParameters();
+            dbparams.Add("Id", data.idCUstomer, DbType.Int32);
+            var result = await Task.FromResult(_dapper.Insert<int>("[dbo].[SP_Add_Article]"
+                , dbparams,
+                commandType: CommandType.StoredProcedure));
+            return result;
+        }
+        [HttpGet(nameof(GetById))]
+        public async Task<Customer> GetById(int Id)
+        {
+            var result = await Task.FromResult(_dapper.Get<Customer>($"Select * from [Dummy] where Id = {Id}", null, commandType: CommandType.Text));
+            return result;
+        }
+        [HttpDelete(nameof(Delete))]
+        public async Task<int> Delete(int Id)
+        {
+            var result = await Task.FromResult(_dapper.Execute($"Delete [Dummy] Where Id = {Id}", null, commandType: CommandType.Text));
+            return result;
+        }
+        [HttpGet(nameof(Count))]
+        public Task<int> Count(int num)
+        {
+            var totalcount = Task.FromResult(_dapper.Get<int>($"select COUNT(*) from [Dummy] WHERE Age like '%{num}%'", null,
+                    commandType: CommandType.Text));
+            return totalcount;
+        }
+        [HttpPatch(nameof(Update))]
+        public Task<int> Update(Customer data)
+        {
+            var dbPara = new DynamicParameters();
+            dbPara.Add("Id", data.idCUstomer);
+            dbPara.Add("Name", data.customerName, DbType.String);
+
+            var updateArticle = Task.FromResult(_dapper.Update<int>("[dbo].[SP_Update_Article]",
+                            dbPara,
+                            commandType: CommandType.StoredProcedure));
+            return updateArticle;
+        }
+    }
+}
---
+++ FitProAPI/Models/Customer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FitProAPI.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int idCUstomer { get; set; }
+        public string customerName { get; set; }
+        public int customerPhone { get; set; }
+        public string customerEmail { get; set; }
+        public string notes { get; set; }
+    }
+}
